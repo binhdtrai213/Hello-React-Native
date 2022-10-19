5971c198205f8b97acd3a72648d7f0abf8bded6f
Binhdt <binhthan13579113@gmail.com>
2022-10-19 22:22:08 +0700

    Create full testing say hello on react native

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
+
+function popupNotification (msg) {
+  alert(msg);
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello react Native</Text>
+      <Text>Your name: </Text>
+      <TextInput
+        defaultValue="Dao Thien Binh"
+        style={{ borderColor: "black", borderWidth: 1, padding: 5 }}
+      />
+      <Button
+        onPress={() => popupNotification('Saved')}
+        title="save"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      
+      <TouchableOpacity>
+        <Text>Save</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
